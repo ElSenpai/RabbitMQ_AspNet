f091862c2bae053a363bb67399536f3c9c9b889d
El Senpai <El Senpai@EL-SENPAI>
2021-05-13 19:56:13 +0300

    Topic Exchange

---
+++ TopicExchange/Publisher/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQ.publisher
+{
+    public enum LogNames
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Info = 4
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://wnkuwlnn:qBNamzSHcGbJW2KtDYRZ-Lmk_Mw69jLT@fish.rmq.cloudamqp.com/wnkuwlnn");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+
+            channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
+
+
+           
+              Random rnd = new Random();
+            Enumerable.Range(1, 50).ToList().ForEach(c =>
+            {
+                
+                LogNames log1 = (LogNames)rnd.Next(1, 5);
+                LogNames log2 = (LogNames)rnd.Next(1, 5);
+                LogNames log3 = (LogNames)rnd.Next(1, 5);
+
+
+                var routeKey = $"{log1}.{log2}.{log3}";
+                string message = $"log-type: {log1}-{log2}-{log3} ";
+                var messagebody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs-topic", routeKey, null, messagebody);
+                Console.WriteLine($"Log gönderilmiştir : {message}");
+            });
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TopicExchange/Subscriber/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://wnkuwlnn:qBNamzSHcGbJW2KtDYRZ-Lmk_Mw69jLT@fish.rmq.cloudamqp.com/wnkuwlnn");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            var queueName = channel.QueueDeclare().QueueName; //channel dan alıcaz
+
+            var routeKey = "Info.#"; //"*.*.Warning"
+
+            channel.QueueBind(queueName,"logs-topic",routeKey);
+
+
+
+            channel.BasicConsume(queueName, false, consumer);
+
+            Console.WriteLine("loglar dinleniyor");
+
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500);
+
+                Console.WriteLine("gelen mesaj:" + message);
+
+                // File.AppendAllText("log-critical.txt", message +"\n");
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
