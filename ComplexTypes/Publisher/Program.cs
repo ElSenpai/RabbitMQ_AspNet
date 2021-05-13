26253c003839d2706bdea734b38fdaa256d16687
El Senpai <El Senpai@EL-SENPAI>
2021-05-13 22:31:29 +0300

    ComplexTypes

---
+++ ComplexTypes/Publisher/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace RabbitMQ.publisher
+{
+    public enum LogNames
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Info = 4
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://wnkuwlnn:qBNamzSHcGbJW2KtDYRZ-Lmk_Mw69jLT@fish.rmq.cloudamqp.com/wnkuwlnn");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+
+            channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+            Dictionary<string, object> headers = new Dictionary<string, object>();
+
+            headers.Add("format", "pdf");
+            headers.Add("shape2", "a4");
+
+
+            var properties = channel.CreateBasicProperties();
+            properties.Headers = headers;
+            properties.Persistent = true; // msjlar kalıcı hale gelir
+
+            var product = new Product { Id=1,Name="Kalem",Price=100,Stock=10};
+
+            var productJsonString = JsonSerializer.Serialize(product);
+
+            channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes(productJsonString));
+
+
+            Console.WriteLine("mesaj gönderilmiştir");
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
