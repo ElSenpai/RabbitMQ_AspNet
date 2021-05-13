8de9609712140765d5e6df5546742cf6fe7a0481
El Senpai <El Senpai@EL-SENPAI>
2021-05-13 17:34:27 +0300

    Add project files.

---
+++ RabbitMQ.Fanout/RabbitMQ.publisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMQ.publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://wnkuwlnn:qBNamzSHcGbJW2KtDYRZ-Lmk_Mw69jLT@fish.rmq.cloudamqp.com/wnkuwlnn");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //channel.QueueDeclare("hello-queue", true, false, false); 
+            channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+
+
+
+
+            Enumerable.Range(1, 50).ToList().ForEach(c =>
+            {
+                string message =  $"log {c} ";
+
+                var messagebody = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("logs-fanout","", null, messagebody);
+                 Console.WriteLine($"Mesaj gönderilmiştir : {message}");
+            });
+
+           
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMQ.Fanout/RabbitMQ.subscriber/Program.cs
@@ -0,0 +1,58 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://wnkuwlnn:qBNamzSHcGbJW2KtDYRZ-Lmk_Mw69jLT@fish.rmq.cloudamqp.com/wnkuwlnn");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //channel.QueueDeclare("hello-queue", true, false, false);
+
+            //channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+
+           // var randomQueueName = "log-database-save-queue";    //channel.QueueDeclare().QueueName;
+            var randomQueueName = channel.QueueDeclare().QueueName;
+          
+           // channel.QueueDeclare(randomQueueName,true,false,false);// ilgili subs kapansa bile bu kuyruk durur.
+
+            channel.QueueBind(randomQueueName,"logs-fanout","",null); // o nedenle bind kullandık ki silinsin
+
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume(randomQueueName,false, consumer);
+
+            Console.WriteLine("loglar dinleniyor");
+
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500);
+
+                Console.WriteLine("gelen mesaj:" +message);
+
+                channel.BasicAck(e.DeliveryTag,false);
+            };
+
+            Console.ReadLine();
+
+        }
+
+       
+    }
+}
