d9e004b7138c9379e0e36d6384eb7d12c40c684f
El Senpai <El Senpai@EL-SENPAI>
2021-05-13 17:47:30 +0300

    direct

---
+++ RabbitMQ.Direct/RabbitMQ.publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQ.publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
